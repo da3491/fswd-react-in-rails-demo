625abbe3351a57b7190d1fa7f2351f1d5891542f
xiaopow <xiaoyang88@gmail.com>
2022-03-24 17:42:07 +0800

    basic react app

---
+++ app/javascript/src/layout.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Layout = (props) => {
+  return (
+    <React.Fragment>
+      <nav className="navbar navbar-expand navbar-light bg-light">
+        <div className="container">
+          <a href="/"><span className="navbar-brand mb-0 h1">React in Rails Demo</span></a>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="/">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/demo">Demo</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="container py-3">
+        {props.children}
+      </div>
+      <footer className="p-3 bg-light">
+        <div>
+          <span className="me-3 text-secondary">Built by <a href="https://www.altcademy.com" target="_blank" rel="noopener noreferrer">Altcademy</a> with ☕ and 💜</span>
+        </div>
+      </footer>
+    </React.Fragment>
+  );
+}
+
+export default Layout;
